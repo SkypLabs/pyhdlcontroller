d57ae46344f0a946f696fb4198eaccc760d4535d
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-12-30 15:10:37 +0100

    use one thread per data frame to send (refactoring)

--- hdlcontroller.py
+++ hdlcontroller.py
@@ -1,6 +1,5 @@
 from yahdlc import *
-from threading import Thread
-from queue import Queue, Empty
+from threading import Thread, Event, Lock
 from time import sleep
 
 class HDLController:
@@ -8,16 +7,9 @@ class HDLController:
 	An HDLC controller based on python4yahdlc.
 	"""
 
-	max_seq_no = 8
+	MAX_SEQ_NO = 8
 
 	def __init__(self, read_func, write_func, window=3):
-		self.window = window
-		self.data_to_send = list()
-		self.ack_to_send = Queue(self.window)
-		self.data_to_resend = Queue(self.window)
-		self.new_seq_no = 0
-		self.next_seq_no = 0
-
 		if not hasattr(read_func, '__call__'):
 			raise TypeError('The read function is not a callable object')
 		if not hasattr(write_func, '__call__'):
@@ -26,113 +18,165 @@ class HDLController:
 		self.read = read_func
 		self.write = write_func
 
-		t_sender = Thread(target=self.__sender)
-		t_receiver = Thread(target=self.__receiver)
+		self.window = window
+		self.senders = list()
+		self.send_lock = Lock()
+		self.new_seq_no = 0
 
-		t_sender.start()
-		t_receiver.start()
+		self.receiver = self.Receiver(self.read, self.write, self.send_lock, self.senders)
+
+	def start(self):
+		"""
+		Start HDLC controller's threads.
+		"""
+
+		self.receiver.start()
+
+	def stop(self):
+		"""
+		Stop HDLC controller's threads.
+		"""
+
+		self.receiver.join()
+
+		for s in self.senders:
+			s.join()
 
 	def send(self, data):
 		"""
 		Send a new data frame.
 
 		This method will block until a new room is
-		available in the queue. The size of the queue
-		is determined by the size of the window.
+		available for a new sender. This limit is
+		determined by the size of the window.
 		"""
 
-		while self.data_to_send.count() >= self.window:
+		while len(self.senders) >= self.window:
 			pass
 
-		self.data_to_send.insert(self.new_seq_no, data)
-		self.new_seq_no = (self.new_seq_no + 1) % HDLController.max_seq_no
+		self.senders.insert(self.new_seq_no, self.Sender(self.write, self.send_lock, data, self.new_seq_no))
+		self.senders[self.new_seq_no].start()
+		self.new_seq_no = (self.new_seq_no + 1) % HDLController.MAX_SEQ_NO
 
-	def __send_data(self, data, seq_no):
-		"""
-		Send a new data frame.
-		"""
-
-		self.write(frame_data(data, FRAME_DATA, seq_no))
-
-	def __send_ack(self, seq_no):
-		"""
-		Send a new ack frame.
-		"""
-
-		self.write(frame_data('', FRAME_ACK, seq_no))
-
-	def __send_nack(self, seq_no):
-		"""
-		Send a new nack frame.
-		"""
-
-		self.write(frame_data('', FRAME_NACK, seq_no))
-
-	def __sender(self):
+	class Sender(Thread):
 		"""
 		Thread used to send HDLC frames.
 		"""
 
-		while True:
-			# Data to resend ?
-			try:
-				for i in range(self.window):
-					seq_no = self.data_to_resend.get_nowait()
-					data = self.data_to_send[seq_no]
-					self.__send_data(data, seq_no)
-			except Empty:
+		def __init__(self, write_func, send_lock, data, seq_no):
+			super().__init__()
+			self.write = write_func
+			self.send_lock = send_lock
+			self.data = data
+			self.seq_no = seq_no
+			self.ack = Event()
+
+		def run(self):
+			with self.send_lock:
+				self.__send_data()
+
+			while not self.ack.isSet():
 				pass
 
-			# Ack to send ?
-			try:
-				for i in range(self.window):
-					type, seq_no = self.ack_to_send.get_nowait()
+		def join(self, timeout=None):
+			"""
+			Stop the current thread.
+			"""
 
-					if type == FRAME_ACK:
-						self.__send_ack(seq_no)
-					elif type == FRAME_NACK:
-						self.__send_nack(seq_no)
-					else:
-						raise TypeError('Bad frame type')
-			except Empty:
-				pass
+			self.ack.set()
+			super().join(timeout)
 
-			# Data to send ?
-			try:
-				data = self.data_to_send[self.next_seq_no]
-				self.__send_data(data, self.next_seq_no)
-				self.next_seq_no = (self.last_sent + 1) % HDLController.max_seq_no
-			except IndexError:
-				pass
+		def ack_received(self):
+			"""
+			Inform the sender that the ack frame
+			has been received which has the
+			consequence of stopping the current
+			thread.
+			"""
 
-	def __receiver(self):
+			self.join()
+
+		def nack_received(self):
+			"""
+			Inform the sender that a nack frame
+			has been received which has the
+			consequence of resending the data
+			frame.
+			"""
+
+			with self.send_lock:
+				self.__send_data()
+
+		def __send_data(self):
+			"""
+			Send a new data frame.
+			"""
+
+			self.write(frame_data(self.data, FRAME_DATA, self.seq_no))
+
+	class Receiver(Thread):
 		"""
 		Thread used to receive HDLC frames.
 		"""
 
-		while True:
-			try:
-				data, type, seq_no = get_data(self.read())
+		def __init__(self, read_func, write_func, send_lock, senders_list):
+			super().__init__()
+			self.read = read_func
+			self.write = write_func
+			self.send_lock = send_lock
+			self.senders = senders_list
+			self.stop_receiver = Event()
 
-				if type == FRAME_DATA:
-					# Send ACK
-					self.ack_to_send.put((FRAME_ACK, (seq_no + 1) % HDLController.max_seq_no))
-				elif type == FRAME_ACK:
-					del self.data_to_send[seq_no]
-				elif type == FRAME_NACK:
-					self.data_to_resend.put(seq_no)
-				else:
-					raise TypeError('Bad frame type received')
-			except MessageError:
-				pass
-			except IndexError:
-				pass
-			except FCSError:
-				# Send NACK
-				self.ack_to_send.put((FRAME_NACK, seq_no))
-			finally:
-				# 200 µs
-				sleep(200 / 1000000.0)
+		def run(self):
+			while not self.stop_receiver.isSet():
+				try:
+					data, type, seq_no = get_data(self.read())
+
+					if type == FRAME_DATA:
+						with self.send_lock:
+							self.__send_ack((seq_no + 1) % HDLController.MAX_SEQ_NO)
+					elif type == FRAME_ACK:
+						seq_no_sent = (seq_no - 1) % HDLController.MAX_SEQ_NO
+						self.senders[seq_no_sent].ack_received()
+						del self.senders[seq_no_sent]
+					elif type == FRAME_NACK:
+						self.senders[seq_no].nack_received()
+					else:
+						raise TypeError('Bad frame type received')
+				except MessageError:
+					# No HDLC frame detected
+					pass
+				except IndexError:
+					# Drop bad (n)ack
+					pass
+				except FCSError:
+					with self.send_lock:
+						self.__send_nack(seq_no)
+				finally:
+					# 200 µs
+					sleep(200 / 1000000.0)
+
+		def join(self, timeout=None):
+			"""
+			Stop the current thread.
+			"""
+
+			self.stop_receiver.set()
+			super().join(timeout)
+
+		def __send_ack(self, seq_no):
+			"""
+			Send a new ack frame.
+			"""
+
+			self.write(frame_data('', FRAME_ACK, seq_no))
+
+		def __send_nack(self, seq_no):
+			"""
+			Send a new nack frame.
+			"""
+
+			self.write(frame_data('', FRAME_NACK, seq_no))
 
 if __name__ == '__main__':
 	import serial
@@ -155,4 +199,11 @@ if __name__ == '__main__':
 	def read_uart():
 		return ser.read(ser.inWaiting())
 
-	hdlc_c = HDLController(read_uart, ser.write)
+	try:
+		hdlc_c = HDLController(read_uart, ser.write)
+		hdlc_c.start()
+		while True: pass
+	except KeyboardInterrupt:
+		stdout.write('[*] Bye !\n')
+		hdlc_c.stop()
+		ser.close()
