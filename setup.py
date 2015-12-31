79faf3a9621264faa6581e700d0a19399c258d22
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-12-31 10:31:34 +0100

    add 'setup.py' file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	name = 'hdlcontroller',
+	version = '0.1.0',
+	description = 'HDLC controller',
+	license = 'MIT',
+	keywords = 'hdlc',
+	author = 'Paul-Emmanuel Raoul',
+	author_email = 'skyper@skyplabs.net',
+	url = 'https://github.com/SkypLabs/python-hdlc-controller',
+	py_modules = ['hdlcontroller'],
+	install_requires = ['python4yahdlc>=1.0.0'],
+)
