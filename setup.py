e3292ca598d89692cc2f0f62d76f1c665f6583f5
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2018-05-08 16:29:28 +0100

    update 'setup.py'

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
-from os.path import dirname, abspath, join
 from codecs import open as fopen
+from os.path import dirname, abspath, join
+from setuptools import setup, find_packages
+
+from hdlcontroller.version import VERSION
 
 DIR = dirname(abspath(__file__))
-VERSION = '0.4.0'
 
 with fopen(join(DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
@@ -25,15 +27,27 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
     ],
-    py_modules = ['hdlcontroller'],
-    scripts = ['hdlcontroller.py'],
-    install_requires = ['python4yahdlc>=1.1.0', 'pyserial>=3.0'],
+    packages = find_packages(),
+    scripts = ['bin/hdlc_test'],
     test_suite = 'test',
+    install_requires = [
+        'python4yahdlc >= 1.1.0',
+        'pyserial >= 3.0'
+    ],
+    extras_require = {
+        'docs': [
+            'sphinx >= 1.4.0',
+            'sphinxcontrib-seqdiag >= 0.8.5',
+            'sphinx-argparse >= 0.2.2',
+            'sphinx_rtd_theme',
+        ],
+    },
 )
