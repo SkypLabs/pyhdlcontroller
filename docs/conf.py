22822b193f921b88a757087bb1a060a90f9d761d
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2023-08-19 14:34:45 +0200

    Update the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -91,7 +91,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -205,6 +205,6 @@ html_context = {
     "display_github": True,  # Integrate GitHub
     "github_user": "SkypLabs",  # Username
     "github_repo": "python-hdlc-controller",  # Repo name
-    "github_version": "master",  # Version
+    "github_version": "main",  # Version
     "conf_py_path": "/docs/",  # Path in the checkout to the docs root
 }
